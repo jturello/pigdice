db5d8bab7a4e8c0b2be69d72d2e6588f6aa85d63
Blake Scott and James Turello <jturello@yahoo.com>
2016-01-27 09:48:22 -0800

    add failing spec for condition Game creates player 1 and player 2

--- spec/specs.js
+++ spec/specs.js
@@ -3,5 +3,15 @@ describe('Player', function() {
     var testPlayer = new Player("Bill");
     expect(testPlayer.name).to.equal("Bill");
   });
+});
 
+
+
+describe('Game', function() {
+  it("creates player 1 and player 2", function() {
+    var player1 = new Player("Bill");
+    var player2 = new Player("Tom");
+    expect(Game.player1).to.equal("Bill");
+    expect(Game.player2).to.equal("Tom");
+  });
 });
