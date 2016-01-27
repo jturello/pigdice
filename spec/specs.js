c457c4754d92d1e815d4c0ab214f7b2cb1653fad
Blake Scott and James Turello <jturello@yahoo.com>
2016-01-27 10:38:05 -0800

    add failing spec for condition scores for player 1 and 2 are initialized to 0

--- spec/specs.js
+++ spec/specs.js
@@ -14,4 +14,16 @@ describe('Game', function() {
     expect(player1.name).to.equal("Bill");
     expect(player2.name).to.equal("Tom");
   });
+
+  it("sets player1 as the active player", function() {
+    new Game("Bill", "Tom");
+    expect(Game.activePlayer).to.equal(Game.player1);
+  });
+
+  it("sets scores for player1 and player2 to zero", function() {
+    new Game("Bill", "Tom");
+    expect(player1.score).to.equal(0);
+    expect(player2.score).to.equal(0);
+  });
+
 });
